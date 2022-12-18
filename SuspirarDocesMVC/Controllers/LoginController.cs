fc9b43b0df85bf6af7029e7c3fe6e167de9910a3
Gabriel Barbosa Bizerra <gabrielbarbosabizerra@gmail.com>
2022-12-18 19:26:15 -0300

    Criação da tela de Login.

---
+++ SuspirarDocesMVC/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuspirarDocesMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Logar(string email, string senha)
+        {
+            return View();
+        }
+    }
+}
